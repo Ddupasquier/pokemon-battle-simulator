73c0ff459ce04443aeddbe5b6431eea3b28246fa
dylandupasquier <dylandupasquier@gmail.com>
2022-07-14 18:02:59 -0700

    testing

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,21 +1,44 @@
 import React, { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useOutletContext } from 'react-router-dom';
 import battleMusic from './Components/controls/assets/battle-music.mp3';
 import Footer from './Components/Footer';
 import Nav from './Components/Nav';
 import './scss/app-styles.css';
 
+type AudioPlayerContext = {
+  audio: HTMLAudioElement
+  isPlaying: boolean
+  playAudio: () => void
+  pauseAudio: () => void
+  toggleAudio: () => void
+}
+
 const App = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [audio] = useState(new Audio(battleMusic));
 
+  const playAudio = () => {
+    audio.play();
+    setIsPlaying(true);
+  };
+
+  const pauseAudio = () => {
+    audio.pause();
+    setIsPlaying(false);
+  };
+
+  const toggleAudio = isPlaying ? pauseAudio : playAudio;
+
   return (
-      <div className="App">
-        <Nav />
-        <Outlet context={[audio, isPlaying, setIsPlaying]} />
-        <Footer />
-      </div>
+    <div className="App">
+      <Nav />
+      <Outlet context={{toggleAudio}} />
+      <Footer />
+    </div>
   );
 };
 
 export default App;
+export function useAudioPlayer() {
+  return useOutletContext<AudioPlayerContext>();
+}
--- ui/src/Components/controls/AudioPlayer.jsx
+++ ui/src/Components/controls/AudioPlayer.jsx
@@ -1,27 +1,36 @@
 import React, { useEffect } from 'react';
 import * as BsIcon from 'react-icons/bs';
-import { useOutletContext } from 'react-router-dom';
+// import { useOutletContext } from 'react-router-dom';
+import {useAudioPlayer} from './../../App';
 
 const AudioPlayer = () => {
-  const [audio, isPlaying, setIsPlaying] = useOutletContext();
+  // const [audio, isPlaying, setIsPlaying] = useOutletContext();
 
+
+  // useEffect(() => {
+  //   audio.play();
+  //   setIsPlaying(true);
+  // }, [audio, setIsPlaying]);
+
+  // const handlePlay = () => {
+  //   if (!isPlaying) {
+  //     audio.play();
+  //     setIsPlaying(true);
+  //   } else {
+  //     audio.pause();
+  //     setIsPlaying(false);
+  //   }
+  // };
+  // const { isPlaying, playAudio, toggleAudio } = useOutletContext();
+
+  const { isPlaying, playAudio, toggleAudio } = useAudioPlayer();
+  
   useEffect(() => {
-    audio.play();
-    setIsPlaying(true);
-  }, [audio, setIsPlaying]);
-
-  const handlePlay = () => {
-    if (!isPlaying) {
-      audio.play();
-      setIsPlaying(true);
-    } else {
-      audio.pause();
-      setIsPlaying(false);
-    }
-  };
+    playAudio();
+  }, [playAudio]);
 
   return (
-    <button className="audio-player" onClick={handlePlay}>
+    <button className="audio-player" onClick={toggleAudio}>
       {isPlaying ? <BsIcon.BsPauseFill /> : <BsIcon.BsFillPlayFill />}
     </button>
   );
